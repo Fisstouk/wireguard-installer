67500e6c68973dd3581bd5db8d9c69bc9b7f94bf
Fisstouk <lyronn.levy@gmail.com>
2023-08-16 23:26:50 +0200

    Improving peer conf file

--- wireguard_server.sh
+++ wireguard_server.sh
@@ -3,7 +3,9 @@
 wireguard_bin="/usr/bin/wg"
 ip_address_server="192.168.2.1/29"
 ip_peer_address="192.168.2.2/29"
+ip_network_id="192.168.2.0/29"
 listen_port="51820"
+network_interface="enp0s3"
 
 echo "Updating the system..."
 echo
@@ -47,10 +49,10 @@ sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/" /etc/sysctl.conf
 # firewall rules
 
 cat >> /etc/wireguard/wg0.conf << EOF
-PostUp = ufw route allow in on wg0 out on eth0
-PostUp = iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
-PreDown = ufw route delete allow in on wg0 out on eth0
-PreDown = iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+PostUp = ufw route allow in on wg0 out on ${network_interface} 
+PostUp = iptables -t nat -I POSTROUTING -o ${network_interface} -j MASQUERADE
+PreDown = ufw route delete allow in on wg0 out ${network_interface} eth0
+PreDown = iptables -t nat -D POSTROUTING -o ${network_interface} -j MASQUERADE
 EOF
 
 # allow ssh
@@ -77,11 +79,16 @@ private_key_peer=$(cat privatekey_peer)
 publick_key_peer=$(cat publickey_peer)
 
 # peer configuration file
+# endpoint will be the public ip address
 
 cat > /etc/wireguard/peer.conf << EOF
 [Interface]
 PrivateKey = ${private_key_peer}
 Address = ${ip_address_peer}
 ListenPort = ${listen_port}
-SaveConfig = true
+
+[Peer]
+PublicKey = ${public_key_server}
+AllowedIPs = ${ip_network_id}
+Endpoint = 100.100.100.100:51820
 EOF
